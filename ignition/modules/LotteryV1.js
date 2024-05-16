6bfe1a316a8081520145567f9a5c935b3e29ab68
akaMiWP <bangkokappdevelopment@gmail.com>
2024-05-16 20:03:43 +0700

    Updated deployment ignition script

--- ignition/modules/LotteryV1.js
+++ ignition/modules/LotteryV1.js
@@ -2,9 +2,10 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const { ethers } = require("hardhat");
 
 module.exports = buildModule("LotteryV1", (m) => {
-  const signers = ethers.getSigners();
-  const deployer = signers[0];
+  // const signers = ethers.getSigners();
+  // const deployer = signers[0];
 
-  const lotteryV1 = m.contract("LotteryV1", deployer);
+  // const lotteryV1 = m.contract("LotteryV1", deployer);
+  const lotteryV1 = m.contract("LotteryV1");
   return { lotteryV1 };
 });
