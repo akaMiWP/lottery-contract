f0829868420ee8fd63e9ff73a65aac389523eb67
akaMiWP <bangkokappdevelopment@gmail.com>
2024-05-25 10:48:26 +0700

    Added index.js script to interact with the deployed contract

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,19 @@ const { ethers } = require("hardhat");
 
 async function main() {
   const [deployer, user] = await ethers.getSigners();
+  console.log("Deployer address", await deployer.getAddress());
+  console.log("User address", await user.getAddress());
+
   const LotteryV1 = await ethers.getContractFactory("LotteryV1");
   const lotteryV1 = await LotteryV1.attach(
     "0x5fbdb2315678afecb367f032d93f642f64180aa3"
   );
+  const lotteryV1WithUserAsASigner = lotteryV1.connect(user);
+  const tx = await lotteryV1.depositToThePool({
+    value: ethers.parseUnits("1", "ether"),
+  });
+
+  console.log("Transaction successful", tx.hash);
 }
+
+main().catch(console.error);
